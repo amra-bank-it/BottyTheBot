a498b44d7c4c49c9ddccbd8debc138ed91062914
Dima <Dima@DEVIDE>
2022-09-08 12:11:24 +0300

    Hopefully, a missing file fix

--- BottyTheBot/Program.cs
+++ BottyTheBot/Program.cs
@@ -16,10 +16,10 @@ while (true)
     cmd.ExecuteNonQuery();
     con.Close();
 
-    Download.Loading();
+    //Download.Loading();
     BZ2.Unpacking();
 
-    using (engine.BeginReadFile(@"C:\Users\Dima\source\repos\BottyTheBot\BottyTheBot\bin\Debug\net6.0\list_of_expired_passports.csv"))
+    using (engine.BeginReadFile(@"C:\Users\Dima\source\repos\BottyTheBot\BottyTheBot\bin\Debug\net6.0\list_of_expired_passports_decomp.csv"))
     {
         Console.WriteLine("Производится запись в базу данных, пожалуйста, подождите.");
 
